1987200110cd639111c5845ad7520b1ea24e29b3
sumyeom <wjstndus75@naver.com>
2024-11-08 13:06:25 +0900

    [ScheduleProject] SQL 파일 수정
    1. 게시물 조회 쿼리 추가
    
    2. 페이징네이션 쿼리 추가

--- schedule.sql
+++ schedule.sql
@@ -29,6 +29,19 @@ SELECT * FROM schedule where id = 1;
 
 SELECT * FROM user where uid = '1523b4ab-08a1-49d5-bad8-b854514e873c';
 
+SELECT s.id, u.name, u.email, s.title, s.content, s.created_date, s.updated_date
+FROM schedule s
+JOIN user u ON s.uid = u.uid
+AND (DATE(s.updated_date) = '2024-11-08' OR '2024-11-08' IS NULL)
+AND (u.name = '숨염' OR '숨염' IS NULL
+ORDER BY s.updated_date DESC;
+
+SELECT s.id, u.name, u.email, s.title, s.content, s.created_date, s.updated_date
+FROM schedule s
+JOIN user u ON s.uid = u.uid
+ORDER BY s.updated_date DESC
+LIMIT 4 OFFSET 1 ;
+
 UPDATE schedule SET title = '11월 1일 데일리 스크럼!!' ,content = '1. Spring 강의 듣기 | 2. Lv.0 과제 완료 | 3. 코드 카타 진행하기' where id = 1;
 
 UPDATE user SET name = '점숨염' WHERE uid = '9ccb2fd0-97af-11ef-82ae-0fc9c3770cd3';
